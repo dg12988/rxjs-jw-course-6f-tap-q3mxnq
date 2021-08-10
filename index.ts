55b8aa10c9c1da8948887281de882754fd9cc4f8
dg12988 <doug.goldberg@me.com>
2021-08-10 10:53:01 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import { Observable, of } from "rxjs";
-import { filter, map } from "rxjs/operators";
+import { filter, map, tap } from "rxjs/operators";
 
 // of creates source Observable
 of(1, 7, 3, 6, 2).pipe(
-  map(value => value * 2),
   filter(value => value > 5),  
+  tap(value => console.log('Spy:', value)),
+  map(value => value * 2),  
 ).subscribe(value => console.log('Output:', value));
